c1919081d5585cc1ec1ebf7d90b0ee97e23bdbf7
Naresh Khatri <68143538+nareshKhatri-025@users.noreply.github.com>
2024-09-03 22:54:16 +0530

    Update 03_converesion_operations.js

--- 01_basics/03_converesion_operations.js
+++ 01_basics/03_converesion_operations.js
@@ -1,14 +1,14 @@
 let score=33
 //its a number
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 //output
 // number
 // number
 
 let score2="33"
-console.log(typeof(score2))
-console.log(typeof score2);
+// console.log(typeof(score2))
+// console.log(typeof score2);
 
 //output
 // string
@@ -16,28 +16,28 @@ console.log(typeof score2);
 
 //conversion
 
-console.log(Number(score2));
+// console.log(Number(score2));
 
 //output
 //33
 
 let score3="33abc"
 
-console.log(typeof Number(score3));
-console.log(Number(score3));
+// console.log(typeof Number(score3));
+// console.log(Number(score3));
 //output
 
 // number
 // NaN
 
 score =undefined
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber=Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //output
 
@@ -45,8 +45,27 @@ console.log(valueInNumber);
 // undefined
 // number
 // NaN
+// ==================Boolean Converiosn=======
+let isLogged =true;
+console.log(typeof isLogged);
+isLogged=Boolean(1);
+console.log(typeof isLogged);
+isLogged=Boolean("");
 
+console.log(typeof isLogged);
 
+console.log( isLogged);
+//output
+// boolean
+// boolean
+// boolean
+// false
+
+// ==============String conversion========
+
+stringFromBoolean=String(isLogged);
+
+console.log(stringFromBoolean)
 
 
 
